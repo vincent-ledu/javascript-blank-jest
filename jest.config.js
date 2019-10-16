39583560aedc20b892ca183567da7ebb2eb6e3bb
Vincent Le Dû <vincent-ledu@users.noreply.github.com>
2019-10-16 09:34:13 +0200

    fix console log that doesn't appear in tests

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  clearMocks: true,
+  moduleDirectories: [
+    'node_modules',
+  ],
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__tests__/**/*.js?(x)',
+    '**/?(*.)+(spec|test).js?(x)',
+  ],
+  verbose: false,
+};
